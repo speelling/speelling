d37a286b46288735b485b9fe2fe836e058fc05e7
theodeeney <theo.paraskeva123@gmail.com>
2023-10-01 03:03:22 +0100

    view modularity

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,5 +1,59 @@
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { AnswerProps } from "../types/AnswerProps";
 
-export default function Answer() {
-  return <div>Answer</div>;
-}
+const Answer: React.FC<AnswerProps> = ({
+  round,
+  time,
+  score,
+  showSoundButton,
+  playSound,
+  buttonDisabled,
+  value,
+  spell,
+  setValue,
+  answerStatus,
+  nextTurn,
+  inputRef,
+  buttonText,
+}) => {
+  return (
+    <div>
+      <div className="homt">{`Round  ${round}/10`}</div>
+
+      <div>
+        <div>00:{time.toString().padStart(2, "0")}</div>
+        {score}
+      </div>
+
+      <div className="playsound">
+        {showSoundButton && (
+          <button className="" onClick={playSound} disabled={buttonDisabled}>
+            <FontAwesomeIcon icon={faVolumeUp} />
+          </button>
+        )}
+      </div>
+      <input
+        className="homeinputs"
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        ref={inputRef}
+        readOnly={true}
+        maxLength={spell.length}
+      />
+      <div>{spell}</div>
+
+      <button className="" onClick={nextTurn}>
+        {buttonText}
+      </button>
+
+      <div className={answerStatus === "correct" ? "correct" : "incorrect"}>
+        {answerStatus}
+      </div>
+    </div>
+  );
+};
+
+export default Answer;
---
+++ src/components/Question.tsx
@@ -0,0 +1,66 @@
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { QuestionProps } from "../types/QuestionProps";
+
+const Question: React.FC<QuestionProps> = ({
+  round,
+  time,
+  score,
+  showSoundButton,
+  playSound,
+  buttonDisabled,
+  value,
+  spell,
+  setValue,
+  inputRef,
+  buttonText,
+  newTurn,
+}) => {
+  return (
+    <div>
+      <div className="homt">{`Round  ${round}/10`}</div>
+
+      <div>
+        <div>00:{time.toString().padStart(2, "0")}</div>
+        {score}
+      </div>
+
+      <div className="playsound">
+        {showSoundButton && (
+          <button className="" onClick={playSound} disabled={buttonDisabled}>
+            <FontAwesomeIcon icon={faVolumeUp} />
+          </button>
+        )}
+      </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          newTurn(value);
+        }}
+      >
+        <input
+          className="homeinputs"
+          type="text"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          ref={inputRef}
+          readOnly={true}
+          maxLength={spell.length}
+          autoComplete="off"
+          spellCheck="false"
+          autoCapitalize="off"
+          autoCorrect={"false"}
+        />
+
+        <div className="subbut">
+          <button className="" type="submit">
+            {buttonText}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Question;
